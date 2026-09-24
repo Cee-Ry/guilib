7083d14ec7c2bf275c3961663dd73e49bb6fede2
Cee-Ry <eduaveceasar@gmail.com>
2026-09-25 00:28:23 +0800

    feat: Added Window Class

--- include/guilib.hpp
+++ include/guilib.hpp
@@ -1,6 +1,33 @@
 #pragma once
 
 #include <raylib.h>
+#include <string_view>
 
 namespace gui {
-}
+  // Window Class
+  class Window {
+  public:
+    // Constructor
+    Window(int width, int height, const std::string_view title) {
+      InitWindow(width, height, title.data());
+      SetTargetFPS(60);
+    }
+
+    // Deconstructor
+    ~Window() {
+      CloseWindow();
+    }
+
+    // Methods
+    bool isOpen() const {
+      return !WindowShouldClose();
+    }
+    void startRender(Color setColor) {
+      BeginDrawing();
+      ClearBackground(setColor);
+    }
+    void endRender() {
+      EndDrawing();
+    }
+  }; // Window
+} // namespace 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
-#include <guilib.hpp>
+#include "guilib.hpp"
 
 int main() {
+  gui::Window window(250, 250, "Title");
+  gui::Window win(250, 250, "Title");
+
+  while(window.isOpen()) {
+    window.startRender(BLUE);
+
+    while (win.isOpen()) {
+      win.startRender(WHITE);
+      win.endRender();
+    }
+    window.endRender();
+  }
+
   return 0;
 }
