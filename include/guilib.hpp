6e009361b230969ef4d5aff6cc589fb89ba7e2da
Cee-Ry <eduaveceasar@gmail.com>
2026-09-25 11:02:53 +0800

    Add stable OOP wrapper for window initialization and frame rendering

--- include/guilib.hpp
+++ include/guilib.hpp
@@ -1,33 +1,52 @@
 #pragma once
 
+#include <cstdlib>
+#include <print>
 #include <raylib.h>
 #include <string_view>
 
 namespace gui {
   // Window Class
   class Window {
+  private:
+    inline static bool isInitialized {false};
+
   public:
     // Constructor
     Window(int width, int height, const std::string_view title) {
+      if (isInitialized == true) {
+        std::println(stderr, "\033[31m[GUI Warning] Attempted to open multiple windows. Must only be one [gui::Window]\033[0m");
+        exit(EXIT_FAILURE);
+      }
+
       InitWindow(width, height, title.data());
       SetTargetFPS(60);
+      isInitialized = true;
     }
-
     // Deconstructor
     ~Window() {
       CloseWindow();
     }
 
-    // Methods
     bool isOpen() const {
       return !WindowShouldClose();
     }
-    void startRender(Color setColor) {
-      BeginDrawing();
-      ClearBackground(setColor);
-    }
-    void endRender() {
-      EndDrawing();
+    
+    class Renderer {
+    private:
+    public:
+      Renderer(Color background) {
+        BeginDrawing();
+        ClearBackground(background);
+      }
+      ~Renderer() {
+        EndDrawing();
+      }
+    }; // Renderer
+
+    [[nodiscard]] Renderer startRender(Color background = RAYWHITE) const {
+      return background;
     }
   }; // Window
 } // namespace 
+
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,9 @@
 
 int main() {
   gui::Window window(250, 250, "Title");
-  gui::Window win(250, 250, "Title");
 
   while(window.isOpen()) {
-    window.startRender(BLUE);
-
-    while (win.isOpen()) {
-      win.startRender(WHITE);
-      win.endRender();
-    }
-    window.endRender();
+    auto frame = window.startRender();
   }
 
   return 0;
