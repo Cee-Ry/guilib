91d5ef547c36ff5d63d15a7d8234a6e3efa195e1
Cee-Ry <eduaveceasar@gmail.com>
2026-09-25 19:12:02 +0800

    fix: removed the boolean initfinder, replaced with IsWindowReady()

--- include/guilib.hpp
+++ include/guilib.hpp
@@ -8,20 +8,16 @@
 namespace gui {
   // Window Class
   class Window {
-  private:
-    inline static bool isInitialized {false};
-
   public:
     // Constructor
     Window(int width, int height, const std::string_view title) {
-      if (isInitialized == true) {
+      if (IsWindowReady()) {
         std::println(stderr, "\033[31m[GUI Warning] Attempted to open multiple windows. Must only be one [gui::Window]\033[0m");
         exit(EXIT_FAILURE);
       }
 
       InitWindow(width, height, title.data());
       SetTargetFPS(60);
-      isInitialized = true;
     }
     // Deconstructor
     ~Window() {
